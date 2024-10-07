88b059aff28e0215ae42237dddc3771c2bdb83e9
Menath Nadungoda <nadungmenath1@gmail.com>
2024-10-07 23:29:54 +0530

    Refactor layout.tsx to add a new Layout component for authentication pages

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="w-full min-h-screen bg-fixed bg-center bg-cover flex-center bg-primary-50 bg-dotted-pattern">
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
