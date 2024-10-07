71d7059ebc9132cf03dc3ffd3fd4779b1d593758
Menath Nadungoda <nadungmenath1@gmail.com>
2024-10-08 02:45:43 +0530

    Refactor Footer component to include logo and copyright information

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,5 +1,23 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="border-t">
+      <div className="flex flex-col gap-4 p-5 text-center flex-center wrapper flex-between sm:flex-row">
+        <Link href={"/"}>
+          <Image
+            src={"/assets/images/logo.png"}
+            width={128}
+            height={38}
+            alt="Blogify"
+          />
+        </Link>
+        <p>© 2024 Blogify. All rights reserved.</p>
+        <p>Share Your Story | Shape the World</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
