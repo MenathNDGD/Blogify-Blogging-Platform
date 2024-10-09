f5aed37a29784f4fd506d8dd148763179f1e41e9
Menath Nadungoda <nadungmenath1@gmail.com>
2024-10-10 01:48:09 +0530

    Refactor database connection code

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,25 +1,59 @@
-import mongoose from "mongoose";
+import mongoose, { Mongoose } from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = process.env.MONGODB_URI!;
 
-let cachedConnection = (global as any).mongoose || {
-  conn: null,
-  promise: null,
-};
+interface MongooseConn {
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
+}
+
+let cached: MongooseConn = (global as any).mongoose;
+
+if (!cached) {
+  cached = (global as any).mongoose = {
+    conn: null,
+    promise: null,
+  };
+}
 
 export const connectToDatabase = async () => {
-  if (cachedConnection.conn) return cachedConnection.conn;
+  if (cached.conn) return cached.conn;
 
-  if (!MONGODB_URI) throw new Error("MongoDB URI is missing");
-
-  cachedConnection.promise =
-    cachedConnection.promise ||
+  cached.promise =
+    cached.promise ||
     mongoose.connect(MONGODB_URI, {
       dbName: "blogify",
       bufferCommands: false,
+      connectTimeoutMS: 30000,
     });
 
-  cachedConnection.conn = await cachedConnection.promise;
+  cached.conn = await cached.promise;
 
-  return cachedConnection.conn;
+  return cached.conn;
 };
+
+// import mongoose from "mongoose";
+
+// const MONGODB_URI = process.env.MONGODB_URI;
+
+// let cachedConnection = (global as any).mongoose || {
+//   conn: null,
+//   promise: null,
+// };
+
+// export const connectToDatabase = async () => {
+//   if (cachedConnection.conn) return cachedConnection.conn;
+
+//   if (!MONGODB_URI) throw new Error("MongoDB URI is missing");
+
+//   cachedConnection.promise =
+//     cachedConnection.promise ||
+//     mongoose.connect(MONGODB_URI, {
+//       dbName: "blogify",
+//       bufferCommands: false,
+//     });
+
+//   cachedConnection.conn = await cachedConnection.promise;
+
+//   return cachedConnection.conn;
+// };
