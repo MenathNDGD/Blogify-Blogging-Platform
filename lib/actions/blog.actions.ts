e314c2215b9ca63fa01dab5ac248e20166d8d176
Menath Nadungoda <nadungmenath1@gmail.com>
2024-10-12 12:59:49 +0530

    Refactor createBlog function to use absolute imports and add cache revalidation

--- lib/actions/blog.actions.ts
+++ lib/actions/blog.actions.ts
@@ -3,18 +3,17 @@
 import { CreateBlogParams } from "@/types";
 import { handleError } from "../utils";
 import { connectToDatabase } from "../database";
-import User from "../database/models/user.model";
-import Blog from "../database/models/blog.model";
+import User from "@/lib/database/models/user.model";
+import Blog from "@/lib/database/models/blog.model";
+import { revalidatePath } from "next/cache";
 
-export const createBlog = async ({ blog, userId, path }: CreateBlogParams) => {
+export async function createBlog({ userId, blog, path }: CreateBlogParams) {
   try {
     await connectToDatabase();
 
     const blogger = await User.findById(userId);
 
-    if (!blogger) {
-      throw new Error("Blogger not found");
-    }
+    if (!blogger) throw new Error("Blogger not found");
 
     const newBlog = await Blog.create({
       ...blog,
@@ -22,8 +21,10 @@ export const createBlog = async ({ blog, userId, path }: CreateBlogParams) => {
       blogger: userId,
     });
 
+    revalidatePath(path);
+
     return JSON.parse(JSON.stringify(newBlog));
   } catch (error) {
     handleError(error);
   }
-};
+}
